564a7edc7c07e1c5f7dd3a175a3e9351e526d129
harsh <66548698+harsssh@users.noreply.github.com>
2024-02-14 23:12:33 +0900

    format

--- internal/task/callback_interface.hpp
+++ internal/task/callback_interface.hpp
@@ -1,8 +1,8 @@
 #ifndef CALLBACK_INTERFACE_HPP
 #define CALLBACK_INTERFACE_HPP
 
-#include <string>
 #include "utils/unit.hpp"
+#include <string>
 
 template<typename T, typename E>
 class Result;
@@ -24,7 +24,7 @@ public:
 
 class IWriteFileCallback {
 public:
-   virtual Result<types::Unit, std::string> trigger() = 0;
+    virtual Result<types::Unit, std::string> trigger() = 0;
 };
 
 #endif //CALLBACK_INTERFACE_HPP
