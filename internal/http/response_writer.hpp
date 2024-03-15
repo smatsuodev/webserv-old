5251f889be662ca35ffae86a0447e36cc7095e9a
EringiShimeji <eringi.shimeji.5296@gmail.com>
2024-03-15 18:58:00 +0900

    impl ResponseWriter

---
+++ internal/http/response_writer.hpp
@@ -0,0 +1,85 @@
+#ifndef RESPONSE_WRITER_HPP
+#define RESPONSE_WRITER_HPP
+
+#include "status.hpp"
+#include "task/io_task_manager.hpp"
+#include "task/write_file.hpp"
+#include "utils/unit.hpp"
+#include "utils/utils.hpp"
+#include <sstream>
+#include <unistd.h>
+
+template<class T>
+class ResponseWriter {
+public:
+    static const std::string kProtocolVersion;
+
+    ResponseWriter(IOTaskManager &manager, T output) : manager_(manager), output_(output), status_code_(kStatusOk) {}
+
+    ~ResponseWriter() {}
+
+    void writeBody(const std::string &content) {
+        body_ += content;
+    }
+
+    template<class V>
+    void writeHeader(const std::string &key, V value) {
+        header_ += generateHeaderLine(key, value);
+    }
+
+    void setStatus(HttpStatusCode code) {
+        status_code_ = code;
+    }
+
+    template<class Callback>
+    void send();
+
+private:
+    IOTaskManager &manager_;
+    T output_;
+    HttpStatusCode status_code_;
+    std::string body_;
+    std::string header_;
+
+    std::string generateStatusLine() {
+        std::string status_code = utils::toString(status_code_);
+        std::string status_text = getHttpStatusText(status_code_);
+
+        return kProtocolVersion + " " + status_code + " " + status_text + "\n";
+    }
+
+    template<class V>
+    std::string generateHeaderLine(const std::string &key, V value) {
+        return key + ": " + utils::toString(value) + "\n";
+    }
+
+    template<>
+    std::string generateHeaderLine(const std::string &key, const std::string &value) {
+        return key + ": " + value + "\n";
+    }
+
+    std::string generateRawResponseText() {
+        std::string content_length = generateHeaderLine("Content-Length", body_.size());
+
+        return generateStatusLine() + content_length + header_ + "\r\n" + body_;
+    }
+};
+
+template<class T>
+const std::string ResponseWriter<T>::kProtocolVersion = "HTTP/1.1";
+
+template<>
+template<class Callback>
+void ResponseWriter<int>::send() {
+    new WriteFile(manager_, output_, generateRawResponseText(), new Callback(output_));
+}
+
+// This function is for testing purposes only.
+template<>
+template<class Callback = void>
+void ResponseWriter<std::ostream &>::send() {
+    std::string raw_response = generateRawResponseText();
+    output_ << raw_response;
+}
+
+#endif
---
+++ tests/response_writer_test.cpp
@@ -0,0 +1,67 @@
+#include "fakeit.hpp"
+#include "http/response_writer.hpp"
+#include <gtest/gtest.h>
+
+using namespace fakeit;
+
+
+TEST(ResponseWriterTest, sendToFd) {
+    Mock<IOTaskManager> manager;
+    int fd = 1;
+    ResponseWriter<int> writer(manager.get(), fd);
+
+    Fake(Method(manager, addTask));
+
+    writer.writeBody("Hello, world!");
+    writer.send<WriteFileCallback>();
+
+    Verify(Method(manager, addTask)).Exactly(1);
+}
+
+TEST(ResponseWriterTest, sendShortBodyToOStream) {
+    IOTaskManager manager;
+    std::ostringstream output;
+    ResponseWriter<std::ostream &> writer(manager, output);
+
+    writer.writeBody("Hello, world!");
+    writer.send();
+
+    EXPECT_EQ("HTTP/1.1 200 OK\nContent-Length: 13\n\r\nHello, world!", output.str());
+}
+
+TEST(ResponseWriterTest, sendLongBodyToOStream) {
+    IOTaskManager manager;
+    std::ostringstream output;
+    ResponseWriter<std::ostream &> writer(manager, output);
+
+    std::string long_body(1000, 'a');
+    writer.writeBody(long_body);
+    writer.send();
+
+    std::string expected = "HTTP/1.1 200 OK\nContent-Length: 1000\n\r\n" + long_body;
+    EXPECT_EQ(expected, output.str());
+}
+
+TEST(ResponseWriterTest, sendHeadersToOStream) {
+    IOTaskManager manager;
+    std::ostringstream output;
+    ResponseWriter<std::ostream &> writer(manager, output);
+
+    writer.writeHeader("Content-Type", "text/plain");
+    writer.send();
+
+    std::string expected = "HTTP/1.1 200 OK\nContent-Length: 0\nContent-Type: text/plain\n\r\n";
+    EXPECT_EQ(expected, output.str());
+}
+
+TEST(ResponseWriterTest, sendStatusToOStream) {
+    IOTaskManager manager;
+    std::ostringstream output;
+    ResponseWriter<std::ostream &> writer(manager, output);
+
+    writer.setStatus(HttpStatusCode::kStatusNotFound);
+    writer.send();
+
+    std::string expected = "HTTP/1.1 404 Not Found\nContent-Length: 0\n\r\n";
+    EXPECT_EQ(expected, output.str());
+}
