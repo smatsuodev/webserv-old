19a61182d5aaa598786542333daf3cdf1a734129
EringiShimeji <eringi.shimeji.5296@gmail.com>
2024-03-15 20:33:48 +0900

    fix format

--- internal/utils/utils.hpp
+++ internal/utils/utils.hpp
@@ -1,14 +1,14 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
-#include <string>
 #include <sstream>
+#include <string>
 
 namespace utils {
     const unsigned int kKiB = 1024;        // 1 KiB = 1024 bytes
     const unsigned int kMiB = 1024 * kKiB; // 1 MiB = 1024 KiB
 
-    template <class T>
+    template<class T>
     std::string toString(T value) {
         std::stringstream ss;
         ss << value;
