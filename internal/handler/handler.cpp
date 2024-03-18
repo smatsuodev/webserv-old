7c45bc723a85074cc7a31b8e12e2f022eb7496cf
harsh <66548698+harsssh@users.noreply.github.com>
2024-03-18 18:54:04 +0900

    dummy response

--- internal/handler/handler.cpp
+++ internal/handler/handler.cpp
@@ -4,7 +4,7 @@
 IHandler::~IHandler() {}
 
 Result<types::Unit, std::string> Handler::trigger(IContext *ctx) {
-    const std::string data_to_write = ctx->getRequest().text();
+    const std::string data_to_write = "HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello";
     new WriteFile(ctx, data_to_write, new WriteFileCallback(ctx));
     return Ok(unit);
 }
