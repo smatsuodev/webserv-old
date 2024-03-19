93c0565929a3d37f1745a988e84cb668a06dbf10
Mori062 <01morishi@gmail.com>
2024-03-19 17:33:13 +0900

    fix SEGV

--- cmd/webserv.cpp
+++ cmd/webserv.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-    const std::string &path = (argc != 2) ? argv[1] : Config::kDefaultPath;
+    const std::string &path = (argc == 2) ? argv[1] : Config::kDefaultPath;
     Result<Config, std::string> parse_result = Config::parseConfigFile(path);
     if (parse_result.isErr()) {
         std::cerr << "Failed to parse config file: " << parse_result.unwrapErr() << std::endl;
