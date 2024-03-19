f70f520a9109cbb8cec08a22fc81d8c514adba7a
harsh <66548698+harsssh@users.noreply.github.com>
2024-03-19 20:31:55 +0900

    add error handling

--- internal/utils/utils.cpp
+++ internal/utils/utils.cpp
@@ -19,6 +19,10 @@ bool utils::endsWith(const std::string &str, const std::string &suffix) {
 }
 
 Result<unsigned long, std::string> utils::stoul(const std::string &str) {
+    if (str.empty()) {
+        return Err<std::string>("empty string");
+    }
+
     for (size_t i = 0; i < str.size(); i++) {
         if (!std::isdigit(str[i])) {
             return Err<std::string>("contains non-digit character");
