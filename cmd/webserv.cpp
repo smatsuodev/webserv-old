04ef177b76f9c354e7e7efc397796db724ff6c74
Mori062 <01morishi@gmail.com>
2024-03-23 17:05:25 +0900

    argc > 3

--- cmd/webserv.cpp
+++ cmd/webserv.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
+    if (argc > 3) {
+        std::cerr << "Usage: " << argv[0] << " [config file path]" << std::endl;
+        return 1;
+    }
     const std::string &path = (argc == 2) ? argv[1] : Config::kDefaultPath;
     Result<Config, std::string> parse_result = Config::parseConfigFile(path);
     if (parse_result.isErr()) {
