f01fb4c241c8e67e0e90c85a674e4ec8da521ccb
Mori062 <01morishi@gmail.com>
2024-03-26 17:45:13 +0900

    make config_parser_test file

---
+++ tests/config_parser_test.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "config/config.hpp"
+//#include "config/route_config.hpp"
+//#include "config/virtual_server_config.hpp"
+
+TEST(ConfigParserTest, ParseRouteConfigString) {
+    //std::string config_string = "route_path / { document_root /var/www/html; }";
+    //RouteConfig route_config = RouteConfig::parseRouteConfigString(config_string);
+    //EXPECT_EQ(route_config.getRoutePath(), "/");
+}
