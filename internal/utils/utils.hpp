4c6092b03915bac731ffea717fb752d85368920d
harsh <66548698+harsssh@users.noreply.github.com>
2024-04-03 12:01:23 +0900

    add comment on utils::strnstr

--- internal/utils/utils.hpp
+++ internal/utils/utils.hpp
@@ -28,7 +28,8 @@ namespace utils {
     // NOTE: C++98 では std::strtoull (cstdlib の関数) を使えないので, utils::strtoull は簡単に実装できない
     Result<unsigned long, std::string> stoul(const std::string &str);
 
-    // 標準ライブラリに含まれないので, 自前で実装
+    // std::strnstr は標準ライブラリに含まれないので, 環境によってはビルドできない (少なくとも CI 環境では不可)
+    // そのため, strnstr と同様のものを自作する
     // haystack, needle が NULL の場合は None を返す
     Option<char *> strnstr(const char *haystack, const char *needle, std::size_t len);
 } // namespace utils
