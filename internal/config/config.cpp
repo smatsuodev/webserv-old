442b37fe765b18be0de435f658f542835c8e741c
harsh <66548698+harsssh@users.noreply.github.com>
2024-05-02 00:26:39 +0900

    implement Config::setDefaultErrorPage

--- internal/config/config.cpp
+++ internal/config/config.cpp
@@ -33,26 +33,29 @@ Config Config::parseConfigFile(const std::string &path) {
 
 // Set a default error page like nginx
 // Do nothing if error page is already set
-void Config::setDefaultErrorPage(HttpStatusCode code) {
+void Config::setDefaultErrorPage(const HttpStatusCode code) {
     if (error_pages_.find(code) == error_pages_.end()) {
-        error_pages_[code] = "";
+        const std::string text = utils::toString(code) + " " + getHttpStatusText(code);
+        error_pages_[code] =
+                "<html>"
+                "<head><title>" + text + "</title></head>"
+                "<body>"
+                "<center><h1>" + text + "</h1></center>"
+                "<hr>"
+                "<center>webserv/0.1.0</center>"
+                "</body>"
+                "</html>";
     }
 }
 
 /* getters */
-unsigned int Config::getClientMaxBodySize() const {
-    return client_max_body_size_;
-}
+unsigned int Config::getClientMaxBodySize() const { return client_max_body_size_; }
 
-const std::vector<VirtualServerConfig> &Config::getVirtualServers() const {
-    return virtual_servers_;
-}
+const std::vector<VirtualServerConfig> &Config::getVirtualServers() const { return virtual_servers_; }
 
 // If an error page is not set, generate a default one and store it to reduce resource usage
-const std::string &Config::getErrorPage(HttpStatusCode code) {
-    if (error_pages_.find(code) != error_pages_.end()) {
-        return error_pages_.at(code);
-    }
+const std::string &Config::getErrorPage(const HttpStatusCode code) {
+    if (error_pages_.find(code) != error_pages_.end()) { return error_pages_.at(code); }
     setDefaultErrorPage(code);
     return error_pages_.at(code);
 }
---
+++ tests/default_error_page_test.cpp
@@ -0,0 +1,40 @@
+#include <fakeit.hpp>
+#include <gtest/gtest.h>
+#include "config/config.hpp"
+
+using namespace fakeit;
+
+class DefaultErrorPageTest : public ::testing::Test {
+protected:
+    Config config_;
+};
+
+TEST_F(DefaultErrorPageTest, NotFound) {
+    const std::string result = config_.getErrorPage(kStatusNotFound);
+    const std::string want =
+            "<html>"
+            "<head><title>404 Not Found</title></head>"
+            "<body>"
+            "<center><h1>404 Not Found</h1></center>"
+            "<hr>"
+            "<center>webserv/0.1.0</center>"
+            "</body>"
+            "</html>";
+
+    EXPECT_EQ(result, want);
+}
+
+TEST_F(DefaultErrorPageTest, InternalServerError) {
+    const std::string result = config_.getErrorPage(kStatusInternalServerError);
+    const std::string want =
+            "<html>"
+            "<head><title>500 Internal Server Error</title></head>"
+            "<body>"
+            "<center><h1>500 Internal Server Error</h1></center>"
+            "<hr>"
+            "<center>webserv/0.1.0</center>"
+            "</body>"
+            "</html>";
+
+    EXPECT_EQ(result, want);
+}
